e51cc391aa7497ac7eb797c11bbfd6b50787e900
Stban20 <se.ag.2@hotmail.com>
2020-09-04 22:43:40 +0200

    Commit4

--- js/36_Fetch.js
+++ js/36_Fetch.js
@@ -7,10 +7,10 @@ let usersPage = []
 
 let divUsers = document.querySelector('#divUsers')
 
-fetch('https://reqres.in/api/users')
-    .then(data => data.json())
-    .then(users => {
-        usersPage = users.data
+fetch('https://reqres.in/api/users')            //receive an url and after promises
+    .then(data => data.json())                  //methodo then that catch the data and converted to json
+    .then(users => {                            //here we have the data ready, so, we use another then to use a callback function to save the data in teh variable
+        usersPage = users.data                  //
         console.log(usersPage)
 
         usersPage.map((user, i) =>{
