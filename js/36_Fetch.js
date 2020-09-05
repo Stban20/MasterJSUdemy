85805e17163deb5b4f000d8f05bdc20be491d47b
Stban20 <se.ag.2@hotmail.com>
2020-09-05 11:37:45 +0200

    Commit5

--- js/36_Fetch.js
+++ js/36_Fetch.js
@@ -1,25 +1,27 @@
-'use strict'
+"use strict";
 
 //FETCH (ajax)
 //It is a method to do requests to services / api rest / backend
 
-let usersPage = []
+let usersPage = [];
 
-let divUsers = document.querySelector('#divUsers')
+let divUsers = document.querySelector("#divUsers");
 
-fetch('https://reqres.in/api/users')            //receive an url and after promises
-    .then(data => data.json())                  //methodo then that catch the data and converted to json
-    .then(users => {                            //here we have the data ready, so, we use another then to use a callback function to save the data in teh variable
-        usersPage = users.data                  //
-        console.log(usersPage)
+fetch("https://reqres.in/api/users") //receive an url and after promises
+  .then((data) => data.json()) //methodo then that catch the data and converted to json
+  .then((users) => {
+    //here we have the data ready, so, we use another then to use a callback function to save the data in teh variable
+    usersPage = users.data; //
+    console.log(usersPage);
+    console.log(data);
 
-        usersPage.map((user, i) =>{
+    usersPage.map((user, i) => {
+      let name = document.createElement("h3");
 
-            let name = document.createElement('h3')
+      name.innerHTML = ` ${i}. ${user.first_name} ${user.last_name}`;
+      divUsers.appendChild(name);
+      document.getElementById("pLoading").style.display = "none";
+    });
+  });
 
-            name.innerHTML = ` ${i}. ${user.first_name} ${user.last_name}`
-            divUsers.appendChild(name)
-            document.getElementById('pLoading').style.display = 'none'
-        })
-        
-    })
+fetch("https://reqres.in/api/users").then((data) => data.json()).the;
